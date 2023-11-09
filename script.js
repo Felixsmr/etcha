42a757e4bb9c4a1a3601662aee27cee9dfe09054
Felix <2000fsommer@gmail.com>
2023-11-09 19:24:25 +0100

    workin w buttons

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+let color = 'black'
+
 document.addEventListener("DOMContentLoaded", function(){
     createBoard(16);
+
+    let btnPopup = document.querySelector('#popup');
+    btnPopup.addEventListener('click', function(){
+        let size= getSize();
+        createBoard(size);
+    })
+    
     
 })
 
@@ -13,8 +22,39 @@ function createBoard(size){
 
     for(let i = 0; i<numDivs; i++){
         let div = document.createElement('div');
-        div.style.backgroundColor= 'yellow';
+        div.addEventListener("mouseover",colorDiv)
         board.insertAdjacentElement('beforeend',div);
     }
 
+}
+
+function getSize(){
+    let choice = prompt("Enter drawing board Size: ");
+    let message = document.querySelector('#message')
+    if(choice ==''){
+        message.textContent = 'Please provide a number';
+    }
+    else if(choice < 1 || choice >100){
+        message.textContent="Provide a number between 1 and 100";
+    } else{
+        message.textContent = "You can play"
+        return choice;
+    }
+}
+
+function colorDiv(){
+    if(color == "random"){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    } else{
+        this.style.backgroundColor = 'black'
+    }
+}
+
+function setColor(colorChoice){
+    color = colorChoice;
+}
+
+function resetBoard(){
+    let divs = document.querySelectorAll('div')
+    divs.forEach((div) => div.style.backgroundColor = 'white')
 }
